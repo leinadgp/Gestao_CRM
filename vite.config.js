53188445cea156dde7c521458c0ca9a9f274bee5
Daniel <leinadgp@gmail.com>
2026-05-15 10:57:21 -0300

    fix: corrige caminhos 404 para deploy na vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Gestao_CRM/', // Adicione exatamente o nome do seu repositório aqui
+  base: '/', 
 })
